0f7108af4b8167d142f6c71cb29a24bad919821a
chris-noel-chan <chris@Chriss-MacBook-Pro-4.local>
2013-11-05 16:12:54 -0500

    added different navigation javascript (via Thomas)

--- _site/js/navigation.js
+++ _site/js/navigation.js
@@ -1,19 +1,83 @@
-//declare the class nav-top as a variable that we want to observe (so we can change it's data state)
-var navTop = document.querySelector('.nav-top');
-var navBtn = document. querySelector ('.nav-btn');
+// //declare the class nav-top as a variable that we want to observe (so we can change it's data state)
+// var navTop = document.querySelector('.nav-top');
+// var navBtn = document. querySelector ('.nav-btn');
 
-//observe the class nav-button and in the event of a click, perform a programmed action (x)
+// //observe the class nav-button and in the event of a click, perform a programmed action (x)
 
 
-document.querySelector('.nav-btn').addEventListener('click', function (x) {
-	x.preventDefault();
+// document.querySelector('.nav-btn').addEventListener('click', function (x) {
+// 	x.preventDefault();
 
-	if (navTop.getAttribute('data-state') == 'expanded') {
-		navTop.setAttribute('data-state', 'collapsed');
-		navBtn.setAttribute('data-state', 'disengaged');
-	} else {
-		navTop.setAttribute('data-state', 'expanded');
-		navBtn.setAttribute('data-state', 'engaged');
-		}
+// 	if (navTop.getAttribute('data-state') == 'expanded') {
+// 		navTop.setAttribute('data-state', 'collapsed');
+// 		navBtn.setAttribute('data-state', 'disengaged');
+// 	} else {
+// 		navTop.setAttribute('data-state', 'expanded');
+// 		navBtn.setAttribute('data-state', 'engaged');
+// 		}
 
+// });
+
+
+
+"use strict";
+var closeNav, insideNav, navBtn, navLinks, navTop, openNav, toggleNav, waitToCloseNav;
+
+navTop = $('.nav-top');
+
+navBtn = $('.nav-btn');
+
+navLinks = $('.nav-top a');
+
+insideNav = false;
+
+openNav = function() {
+  navTop.setAttribute('data-state', 'expanded');
+  return navBtn.setAttribute('data-state', 'active');
+};
+
+closeNav = function() {
+  navTop.setAttribute('data-state', 'collapsed');
+  return navBtn.setAttribute('data-state', 'inactive');
+};
+
+toggleNav = function() {
+  if (navTop.getAttribute('data-state') == 'expanded') {
+    return closeNav();
+  } else {
+    return openNav();
+  }
+};
+
+waitToCloseNav = function() {
+  return setTimeout(function() {
+    if (!insideNav) {
+      return closeNav();
+    }
+  }, 100);
+};
+
+navBtn.on('click', function(e) {
+  e.preventDefault();
+  return toggleNav();
+});
+
+navBtn.on('focus', function(e) {
+  insideNav = true;
+  return openNav();
+});
+
+navBtn.on('blur', function(e) {
+  insideNav = false;
+  return waitToCloseNav();
+});
+
+navLinks.on('focus', function(e) {
+  insideNav = true;
+  return openNav();
+});
+
+navLinks.on('blur', function(e) {
+  insideNav = false;
+  return waitToCloseNav();
 });
